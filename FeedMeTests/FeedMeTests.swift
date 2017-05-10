dbc49a38d0c85aef75291c076b55dbc2b1814a34
rhetty <hjw.rhett@qq.com>
2017-05-10 21:39:14 +0800

    new proj

---
+++ FeedMeTests/FeedMeTests.swift
@@ -0,0 +1,36 @@
+//
+//  FeedMeTests.swift
+//  FeedMeTests
+//
+//  Created by 黄嘉伟 on 2017/5/10.
+//  Copyright © 2017年 rhett. All rights reserved.
+//
+
+import XCTest
+@testable import FeedMe
+
+class FeedMeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
